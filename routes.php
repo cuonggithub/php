510628aac513291d5191e77fb7189b9f767820e8
cuonggithub <tranngoccuongth@gmail.com>
2017-04-03 21:39:32 +0900

    revert conflict

--- routes.php
+++ routes.php
@@ -1,11 +1,7 @@
 <?php 
 	
-<<<<<<< HEAD
 	$controllers = array("question" => array("index","newQ","detail","delete"),
 							"calendar" => array("index", "newC", "detail") );
-=======
-	$controllers = array("question" => array("index","newQ","detail","delete"));
->>>>>>> origin/master
 	
 	if(array_key_exists($controller, $controllers)){
 		if(in_array($action, $controllers[$controller])){
